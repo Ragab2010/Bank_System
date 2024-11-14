c8a55502750f87f9b2a368ccf6ea3a53c2921e64
Ragab2010 <ragabmx2002@gmail.com>
2024-11-14 04:17:19 +0200

    core bank manager

---
+++ include/bank.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "person.h"
+#include "account.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <stdexcept>
+#include <optional>
+
+using AccountId_t = int ;
+using Id_t = int ;
+using AccountId_t = int ;
+using Name_t = std::string ;
+using Database_t = std::unordered_map<AccountId_t, std::pair<std::shared_ptr<Person>, std::shared_ptr<Account>>>;
+
+
+
+struct AccountDetails {
+    AccountId_t accountId;
+    Name_t personName;
+    std::chrono::system_clock::time_point lastOperationTime;
+    TypeOfOperation operationType;
+};
+
+//Singleton pattern 
+class Bank{
+public:
+friend class SQLiteManager;
+    static Bank& getInstance();
+
+    int getDatabaseSize() const ;
+
+    void addToDatabase(AccountId_t accountId, std::shared_ptr<Person> person, std::shared_ptr<Account> account) ;
+    const Database_t& getDatabase() const ;
+    void updateUniqueAccountId();
+
+    int createAccount( Name_t personName, std::string nationalIdCard, int balance = 0);
+    bool deleteAccount(AccountId_t accountId) ;
+    int balanceInquiryByAccountId(AccountId_t accountId);
+    std::tuple<AccountId_t ,Name_t , std::pair< time_t ,std::string> , time_t > 
+    showAccountDetails(AccountId_t accountId);
+    int withdrawal(AccountId_t accountId , int withdrawalMoney);
+    int deposit(AccountId_t accountId , int depositMoney);
+    std::vector<std::shared_ptr<Account>> showPersonAccounts( Name_t personName);
+    
+    std::pair< time_t ,std::string> convertLastOperationToTimeAndString( std::pair<std::chrono::system_clock::time_point ,TypeOfOperation >  lastOperationDetails);
+
+    void addAccountFromDatabase(AccountId_t accountId, const std::string& ownerName, int balance,
+                            const std::chrono::system_clock::time_point& creationTime,
+                            const std::pair<std::chrono::system_clock::time_point, TypeOfOperation>& lastOperationDetails);
+
+std::vector<std::shared_ptr<Account>> getAccounts() const;
+private:
+
+    std::shared_ptr<Person> findPersonByNameOrId(const Name_t& personName, const std::string& nationalIdCard);
+    std::shared_ptr<Account> findAccountByName(const Name_t& personName);
+
+    std::shared_ptr<Account> findAccountById(AccountId_t accountId) ;
+    //constructor to make Back singleton
+    Bank();
+    /*this for store account and pointer to Person  by AccountId,
+        why pointer/reference  for Person ? for normalize system ,
+        if there more than account for one person so we store only reference for it 
+        to minimize the storage data  as (primary/foreign keys in databases)
+    */
+    std::unordered_map<AccountId_t ,std::pair<std::shared_ptr<Person>  , std::shared_ptr<Account>> > mDatabase;
+
+    /*this table for store the Person instances*/
+    std::vector<std::shared_ptr<Person>> mPersonTable;
+
+    int mNumberOfAcc;
+    int mUniqueAccountId;
+    int mUniquePersonId;
+
+    // std::unordered_map<Person , std::unordered_map<AccountId_t , Account>> mDatabase;
+    // std::unordered_map<AccountId , Account> mAccountTable;
+    // std::unordered_map<PersonId  , Person > mPersonTable;
+};
---
+++ src/bank.cpp
@@ -0,0 +1,233 @@
+
+
+#include "bank.h"
+
+Bank& Bank::getInstance() {
+    static Bank instance;
+    return instance;
+}
+
+int Bank::getDatabaseSize() const {
+    return mDatabase.size();
+}
+
+void Bank::addToDatabase(AccountId_t accountId, std::shared_ptr<Person> person, std::shared_ptr<Account> account) {
+    mDatabase[accountId] = {person, account};
+}
+
+const Database_t& Bank::getDatabase() const {
+    return mDatabase;
+}
+
+void Bank::updateUniqueAccountId() {
+    mUniqueAccountId = 0;
+    mUniquePersonId = 0;
+
+    for (const auto& [accountId, data] : mDatabase) {
+        // Update the unique account ID to the highest account ID
+        mUniqueAccountId = std::max(mUniqueAccountId, accountId);
+
+        // Update the unique person ID to the highest person ID
+        if (data.first) { // Ensure the person pointer is valid
+            mUniquePersonId = std::max(mUniquePersonId, data.first->getPersonId());
+        }
+    }
+
+    // Increment IDs so the next one to be assigned is unique
+    // ++mUniqueAccountId;
+    // ++mUniquePersonId;
+}
+
+int Bank::createAccount( Name_t personName, std::string nationalIdCard, int balance ) {
+    // Create account
+    auto sharedPtrAccount = std::make_shared<Account>(++mUniqueAccountId, balance);
+    if (!sharedPtrAccount) {
+        --mUniqueAccountId;
+        throw std::runtime_error("Cannot create account");
+    }
+
+    // Check if there is a person with the same name or national ID
+    auto PersonIter = findPersonByNameOrId(personName, nationalIdCard);
+
+
+    // If a person with the same name or national ID exists
+    if (PersonIter !=nullptr) {
+        // Increment the number of accounts for the found person
+        PersonIter->incrementNumAcc(mUniqueAccountId);  // Dereference the shared_ptr to access the Person object
+        mDatabase[mUniqueAccountId] = {PersonIter, sharedPtrAccount};
+    } else {
+        // Create a new person and associate with the account
+        auto sharedPtrPerson = std::make_shared<Person>(mUniquePersonId, std::move(personName), std::move(nationalIdCard) , mUniqueAccountId);
+        ++mUniquePersonId;
+        if (sharedPtrPerson == nullptr) {
+            --mUniquePersonId;
+            throw std::runtime_error("Cannot create person");
+        }
+        mDatabase[mUniqueAccountId] = {sharedPtrPerson, sharedPtrAccount};
+        mPersonTable.push_back(sharedPtrPerson);  // Add new person to the person table
+    }
+    //the  mTimeOfCreation and mLastOperationDetails are set by constructor
+    // Increment the account count
+    mNumberOfAcc++;
+    return mUniqueAccountId;
+}
+
+bool Bank::deleteAccount(AccountId_t accountId) {
+    // Find the account by ID
+    auto pairValueIter = mDatabase.find(accountId); 
+    if (pairValueIter == mDatabase.end()) {
+        throw std::runtime_error("there is no Account by this Id_number");
+    }
+    // Access the Person pointer and call decrementNumAcc
+    if (pairValueIter->second.first) { // Check if shared_ptr<Person> is valid
+        pairValueIter->second.first->decrementNumAcc(accountId);
+    }
+
+    // Reset the Account pointer
+    if (pairValueIter->second.second) { // Check if shared_ptr<Account> is valid
+        pairValueIter->second.second.reset();
+    }
+
+    // Reset the Person pointer if necessary
+    if (pairValueIter->second.first) { // Check if shared_ptr<Person> is valid
+        pairValueIter->second.first.reset();
+    }
+
+    // Erase the map entry
+    mDatabase.erase(pairValueIter);
+    return true;
+}
+int Bank::balanceInquiryByAccountId(AccountId_t accountId) {
+        // Find account by account ID
+    auto pairValueIter = mDatabase.find(accountId);
+    if (pairValueIter == mDatabase.end()){
+        throw std::runtime_error("There is no Account by this Id_number");
+    } 
+    pairValueIter->second.second->setLastOperationDetails( std::chrono::system_clock::now() ,TypeOfOperation::CREATION );
+    return pairValueIter->second.second->getBalance();
+}
+std::vector<std::shared_ptr<Account>> Bank::showPersonAccounts( Name_t personName){
+    auto personSharedPtr = findPersonByNameOrId(personName , "");
+    if(personSharedPtr == nullptr){
+        throw std::runtime_error("there is no Person found by this Name");
+    }
+    auto refAccountIdVector =  personSharedPtr->getAccountIdVector();
+    std::vector<std::shared_ptr<Account>> accounts;
+    for(auto & accountId :refAccountIdVector ){
+        auto iter = findAccountById(accountId);
+        if(iter != nullptr)
+            accounts.push_back(iter);
+    }
+    return accounts;
+}
+
+
+std::tuple<AccountId_t ,Name_t , std::pair< time_t ,std::string> , time_t > 
+Bank::showAccountDetails(AccountId_t accountId){
+    auto accountIdIter = mDatabase.find(accountId);
+    if(accountIdIter == mDatabase.end()){
+        throw std::runtime_error("there is no Account by this Id_number");
+    }
+    std::string personName    = accountIdIter->second.first->getName();
+    auto lastOperationDetails =  convertLastOperationToTimeAndString(accountIdIter->second.second->getLastOperationDetails());
+    auto timeOfAccountCreation= accountIdIter->second.second->getTimeOfAccCreation();
+    return {accountId ,personName , lastOperationDetails  , timeOfAccountCreation};
+}
+
+int Bank::deposit(AccountId_t accountId , int depositMoney){
+    auto accountSharedPtr = findAccountById(accountId);
+    if(accountSharedPtr == nullptr){
+        throw std::runtime_error("there is no Account by this Id_number");
+    }
+    accountSharedPtr->setLastOperationDetails( std::chrono::system_clock::now() ,TypeOfOperation::DEPOSIT );
+    // return (balance + depositMony)
+    return accountSharedPtr->deposit(depositMoney);
+}
+
+int Bank::withdrawal(AccountId_t accountId , int withdrawalMoney){
+    auto accountSharedPtr = findAccountById(accountId);
+    if(accountSharedPtr == nullptr){
+        throw std::runtime_error("there is no Account by this Id_number");
+    }
+    if(accountSharedPtr->getBalance() < withdrawalMoney){
+        throw std::runtime_error("there is no enough money to withdrawal");
+    }
+    accountSharedPtr->setLastOperationDetails( std::chrono::system_clock::now() ,TypeOfOperation::WITHDRAWAL );
+    // return (balance - withdrawalMoney)
+    return accountSharedPtr->withdrawal(withdrawalMoney);
+}
+
+
+std::pair< time_t ,std::string> Bank::convertLastOperationToTimeAndString( std::pair<std::chrono::system_clock::time_point ,TypeOfOperation >  lastOperationDetails){
+    std::string lastOperation;
+    switch (lastOperationDetails.second){
+    case TypeOfOperation::CREATION: lastOperation="CREATION";break;
+    case TypeOfOperation::CHECK_BALANCE: lastOperation="CHECK_BALANCE";break;
+    case TypeOfOperation::DEPOSIT: lastOperation="DEPOSIT";break;
+    case TypeOfOperation::WITHDRAWAL: lastOperation="WITHDRAWAL";break;
+    }
+    return {std::chrono::system_clock::to_time_t(lastOperationDetails.first) , lastOperation};
+}
+
+
+void Bank::addAccountFromDatabase(AccountId_t accountId, const std::string& ownerName, int balance,
+                        const std::chrono::system_clock::time_point& creationTime,
+                        const std::pair<std::chrono::system_clock::time_point, TypeOfOperation>& lastOperationDetails) {
+// Create a new Account
+auto account = std::make_shared<Account>(accountId, balance);
+account->setLastOperationDetails(lastOperationDetails.first, lastOperationDetails.second);
+
+// Check if the person already exists
+auto person = findPersonByNameOrId(ownerName, "");
+if (!person) {
+    person = std::make_shared<Person>(++mUniquePersonId, ownerName, "", accountId);
+    mPersonTable.push_back(person);
+}
+
+// Add to the database
+mDatabase[accountId] = {person, account};
+}
+
+std::vector<std::shared_ptr<Account>> Bank::getAccounts() const {
+std::vector<std::shared_ptr<Account>> accounts;
+for (const auto& entry : mDatabase) {
+    accounts.push_back(entry.second.second);  // Add account (second element of the pair)
+}
+return accounts;
+}
+
+
+
+std::shared_ptr<Person> Bank::findPersonByNameOrId(const Name_t& personName, const std::string& nationalIdCard)
+{
+    for(auto &[accountId, pairValue] : mDatabase) {
+        if ((pairValue.first->getName() == personName) || 
+            (pairValue.first->getNationalIdCard() == nationalIdCard)) {
+            return pairValue.first; // Return the shared_ptr to the Person object
+        }
+    }
+    return nullptr; // Return nullptr if no match is found
+}
+std::shared_ptr<Account> Bank::findAccountByName(const Name_t& personName){
+    for(auto &[accountId, pairValue] : mDatabase) {
+        if ((pairValue.first->getName() == personName) ){
+            return pairValue.second; // Return the shared_ptr to the Person object
+        }
+    }
+    return nullptr; // Return nullptr if no match is found
+}
+
+std::shared_ptr<Account> Bank::findAccountById(AccountId_t accountId) {
+    // Search for the account by its ID in the account database (unordered_map)
+    auto iter = mDatabase.find(accountId);  // mDatabase is assumed to store the account objects
+
+    if (iter != mDatabase.end()) {
+        // Account found, return the shared pointer to the account
+        return iter->second.second;  // second is the shared_ptr<Account> stored in the pair
+    }
+
+    // Account not found, return nullptr
+    return nullptr;
+}
+//constructor to make Back singleton
+Bank::Bank():mDatabase{} ,mPersonTable{} ,mNumberOfAcc{} , mUniqueAccountId{} , mUniquePersonId{}{}
